1d2f3c7f3082bb8973a98fb6264ba8e4273cce27
AshwinDandekar <dandekar.ashwin@gmail.com>
2020-04-12 14:08:21 -0400

    initial master commit

---
+++ src/main/java/com/srtmaker/application/SrtMakerApplication.java
@@ -0,0 +1,12 @@
+package com.srtmaker.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrtMakerApplication {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SrtMakerApplication.class, args);
+    }
+}
